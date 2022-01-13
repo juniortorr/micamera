73214140236b2440b9a25823e7925c662753e0e1
juniortorr <juniortorr77@gmail.com>
2022-01-12 19:48:17 -0800

    home page html inserted

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require('express')
+const express = require('express');
+
 const app = express()
 const port = 3000
+const path = require('path');
 
 app.set('view engine', 'ejs')
+
+//MiddleWares//
+
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//End of Middleware
+
+//Begin Routes 
 
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+//End Routes
 
 
 app.listen(port, () => {
