33f10158eae026683a4f526755f049d9d39a1449
juniortorr <juniortorr77@gmail.com>
2022-03-09 20:20:07 -0800

    api edits

--- public/js/contact.js
+++ public/js/contact.js
@@ -2,7 +2,7 @@
 
 (function() {
     // https://dashboard.emailjs.com/admin/integration
-    emailjs.init('VezWSc9rfmJ_vS_j-');
+    emailjs.init('QISjpQBbhEnGDqTWD');
 })();
 
 window.onload = function() {
@@ -11,7 +11,7 @@ window.onload = function() {
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
-        emailjs.sendForm('service_tihjo6d', 'template_pltnvi1', this)
+        emailjs.sendForm('service_pi5r2bc', 'template_rd4pjrf', this)
             .then(function() {
                 console.log('SUCCESS!');
                 alert('Email Sent!');
